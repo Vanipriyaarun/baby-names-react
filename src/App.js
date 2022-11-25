2072de0ce92aa99b10b60d77191808ddf00f28cf
vanipriya_piramanathan <vpriyaarun23@gmail.com>
2022-11-25 19:56:14 +0000

    level-2 completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React,{ useState } from "react";
 import "./App.css";
 
 const App = (props) => {
+  const [query, setQuery]=useState("");
+
   return (
-    <div className="babyNameCollections">
-      <ul className="nameList">
-        {props.babyNames.sort((a,b)=>(a.name.toUpperCase() < b.name.toUpperCase() ? -1 : a.name.toUpperCase() > b.name.toUpperCase() ? 1 : 0))
-        .map((babyName,index)=>(
-          babyName.sex==="f" ? <li className="babyName" key={index}>{babyName.name}</li> : <li className="boyBabyName" key={index}>{babyName.name}</li>
-        ))};
-      </ul>
+    <div>
+      <input type="text" placeholder="Search for a name..." className="search" onChange={(e)=> setQuery(e.target.value)}/>
+      <div className="babyNameCollections">
+        <ul className="nameList">
+          {props.babyNames.sort((a,b)=>(a.name.toUpperCase() < b.name.toUpperCase() ? -1 : a.name.toUpperCase() > b.name.toUpperCase() ? 1 : 0))
+          .filter((babyName)=>babyName.name.toLowerCase().includes(query)).map((babyName,index)=>(
+            babyName.sex==="f" ? <li className="babyName" key={index}>{babyName.name}</li> : <li className="boyBabyName" key={index}>{babyName.name}</li>
+          ))};
+        </ul>
+      </div>
     </div>
   );
 };
